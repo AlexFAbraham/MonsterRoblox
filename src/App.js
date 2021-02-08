17dd7a96d87e22c52c1c3e11cf307262c27ab3be
Alex Abraham <Alex.f.abraham@gmail.com>
2021-02-08 12:13:11 -0500

    adding from local

---
+++ src/App.js
@@ -0,0 +1,36 @@
+//import logo from './logo.svg';
+import './App.css';
+import { Component } from 'react';
+import {CardList} from './components/card-list/card-list.component';
+
+class App extends Component{
+  constructor(){
+    super()
+    this.state = {
+      monster: []
+    };
+  }
+      componentDidMount() { 
+        fetch('http://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(users => this.setState({monster: users}));
+              }
+  
+
+
+
+  render(){
+    return(
+      <div className='App'>
+      <CardList monster={this.state.monster}>
+     
+      </CardList>
+       
+    
+      </div>
+   );
+  }
+}
+
+
+export default App;
